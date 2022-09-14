e83cb5402e47e9a3a46586677860772dc15df263
jorge niglia <jorge.niglia@gmail.com>
2022-09-14 15:09:09 -0300

    carrito de compras en modal

---
+++ src/components/CartEmpty.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CartEmpty = () => {
+  return (
+    <>
+      <h3>Carrito de compras</h3>
+      <h4>El carrito de compras esta vacío</h4>
+    </>
+  );
+};
+
+export default CartEmpty;
--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,8 +1,10 @@
 import { useContext } from "react";
 import { ShoppingContext } from "../context/ShoppingContext";
+import { useModal } from "../hooks/useModal";
 import CartItem from "./CartItem";
-import Product from "./Product";
-
+import ProductCard from "./ProductCard";
+import Modal from "./Modal";
+import CartEmpty from "./CartEmpty";
 import "./shoppingCart.css";
 import Loading from "./Loading";
 
@@ -10,6 +12,7 @@ const ShoppingCart = () => {
   const data = useContext(ShoppingContext);
   const { loading, products, addToCart, cart, deleteFromCart, cleanCart } =
     data;
+  const [isOpenModal, openModal, closeModal] = useModal(false);
   return (
     <>
       <section>
@@ -18,46 +21,69 @@ const ShoppingCart = () => {
           {loading && <Loading />}
 
           {products.map((product) => (
-            <Product key={product.id} data={product} addToCart={addToCart} />
-          ))}
-        </div>
-      </section>
-
-      <h2 className="my-4 text-xl font-bold text-center">Carrito</h2>
-      <section>
-        <article className="card">
-          {loading && <Loading />}
-          {cart.map((item) => (
-            <CartItem
-              key={item.name}
-              data={item}
-              deleteFromCart={deleteFromCart}
+            <ProductCard
+              key={product.id}
+              data={product}
+              addToCart={addToCart}
             />
           ))}
-        </article>
+        </div>
+        <button onClick={openModal}>Abrir Carrito de compras</button>
       </section>
-
-      <br />
-      <button
-        className="p-1 mt-4 font-bold border-2 border-yellow-500 rounded text-grey-500"
-        onClick={() => cleanCart(cart)}
-      >
-        Limpiar Carrito
-      </button>
-      <br />
-      <h4>
-        {" "}
-        Precio total: $
-        {cart.reduce((previous, current) => {
-          return Number(previous) + Number(current.price * current.quantity);
-        }, 0)}
-      </h4>
-      <h5>
-        Cantidad Total:{" "}
-        {cart.reduce((previous, current) => {
-          return Number(previous) + Number(current.quantity);
-        }, 0)}
-      </h5>
+      <Modal isOpen={isOpenModal} closeModal={closeModal}>
+        {cart.length === 0 && <CartEmpty />}
+        {cart.length !== 0 && (
+          <>
+            <section>
+              <h2 className="my-4 text-xl font-bold text-center">Carrito</h2>
+              <article className="card">
+                {cart.map((item) => (
+                  <CartItem
+                    key={item.name}
+                    data={item}
+                    deleteFromCart={deleteFromCart}
+                  />
+                ))}
+              </article>
+            </section>
+            <section className="flex flex-col items-center">
+              <button
+                className="w-32 p-1 mt-4 font-bold border-2 border-yellow-500 rounded text-grey-500"
+                onClick={() => {
+                  cleanCart(cart);
+                  closeModal();
+                }}
+              >
+                Limpiar Carrito
+              </button>
+              <br />
+              <h4 className="font-bold">
+                {" "}
+                Precio total: ${" "}
+                <span className="text-xl">
+                  {cart.reduce((previous, current) => {
+                    return (
+                      Number(previous) +
+                      Number(current.price * current.quantity)
+                    );
+                  }, 0)}
+                </span>
+              </h4>
+              <h5 className="font-bold">
+                Cantidad Total:{" "}
+                <span className="text-xl">
+                  {cart.reduce((previous, current) => {
+                    return Number(previous) + Number(current.quantity);
+                  }, 0)}
+                </span>
+              </h5>
+              <button className="w-32 p-1 mt-4 font-bold text-center border-2 border-yellow-500 rounded">
+                Comprar
+              </button>
+            </section>
+          </>
+        )}
+      </Modal>
     </>
   );
 };
